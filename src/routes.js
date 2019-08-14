f55b88653b394ace3d92e528e63b5b37e06015b2
Victor Almeida <victooralmeida1@gmail.com>
2019-08-14 10:41:59 -0300

    metodos get removidos

--- src/routes.js
+++ src/routes.js
@@ -5,17 +5,15 @@ const ProductController = require("./controllers/ProductController");
 const UserController = require("./controllers/UserController");
 const DicasHelpSysController = require("./controllers/DicasHelpSysController");
 const AcervoController = require("./controllers/AcervoController");
-routes.get("/products", ProductController.index);
 routes.get("/products/:id", ProductController.show);
 routes.post("/products", ProductController.store);
 routes.put("/products/:id", ProductController.update);
 routes.delete("/products/:id", ProductController.destroy);
-routes.get("/users", UserController.index);
+
 routes.get("/users/:id", UserController.show);
 routes.post("/users", UserController.store);
 routes.put("/users/:id", UserController.update);
 routes.delete("/users/:id", UserController.destroy);
-routes.get("/dicashelpsys", DicasHelpSysController.index);
 routes.get("/dicashelpsys/:id", DicasHelpSysController.show);
 routes.post("/dicashelpsys", DicasHelpSysController.store);
 routes.put("/dicashelpsys/:id", DicasHelpSysController.update);
